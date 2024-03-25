0c83046116f1844e7df38de068bdbe2ff90d8e28
Mirella Rodrigues <MirellaLimaRodrigues@hotmail.com>
2024-03-25 17:45:25 -0300

    AULA 2

--- Aprendendo Programação/Program.cs
+++ Aprendendo Programação/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace AprendendoProgramacao
+﻿using System.Runtime.InteropServices.Marshalling;
+
+namespace AprendendoProgramacao
 {
     internal class Program
     {
@@ -14,9 +16,28 @@
             //decimal Altura = 1.63;
             //bool Escolha = true;
 
-            bool SAIR = true;
+            bool NaoSair = true;
             string Escolha;
             string MeuTexto = "Escolha uma das opções:";
+            string NovoNome;
+            string ExcluirNome;
+            string NomeExistente;
+            string NomePesquisa;
+            bool NomeExiste = false;
+            string NomeEncontrado = "ND";
+
+            List<string> ListaDeNomes = new List<string>();
+
+
+            ListaDeNomes.Add("Mirella");
+            ListaDeNomes.Add("Lucas");
+            ListaDeNomes.Add("Napoleão");
+            ListaDeNomes.Add("Madalena");
+            ListaDeNomes.Add("Rokson");
+            ListaDeNomes.Add("Stark");
+            ListaDeNomes.Add("Pepper");
+            ListaDeNomes.Add("Pepita");
+
             do
             {
                 Console.WriteLine(MeuTexto);
@@ -26,6 +47,7 @@
                 Console.WriteLine("4 - Pesquise um nome.");
                 Console.WriteLine("5 - Consulte todos os nomes.");
                 Console.WriteLine("6 - Sair.");
+                Console.WriteLine("7 - Liste os 5 primeiros nomes.");
                 Console.WriteLine("Sua escolha:");
                 Escolha = Console.ReadLine();
 
@@ -33,14 +55,32 @@
                 {
 
                     case "1":
+                        {
 
-                        Console.WriteLine("Você escolheu a opção: " + Escolha + " - Cadastre um nome.");
+                            Console.WriteLine("Você escolheu a opção: " + Escolha + " - Cadastre um nome.");
+                            Console.WriteLine("Escreva o novo nome:");
 
-                        break;
+                            NovoNome = Console.ReadLine();
+                            ListaDeNomes.Add(NovoNome);
+
+
+                            break;
+
+                        }
 
                     case "2":
 
                         Console.WriteLine("Você escolheu a opção: " + Escolha + " - Altere um nome.");
+
+                        Console.WriteLine("Qual nome você quer Editar?");
+                        NomeExistente = Console.ReadLine();
+
+                        ListaDeNomes.Remove(NomeExistente);
+
+                        Console.WriteLine("Qual o novo nome?");
+                        NovoNome = Console.ReadLine();
+                        ListaDeNomes.Add(NovoNome);
+
 
                         break;
 
@@ -48,11 +88,46 @@
 
                         Console.WriteLine("Você escolheu a opção: " + Escolha + " - Delete um nome.");
 
+                        Console.WriteLine("Escreva o nome para excluir.");
+
+                        ExcluirNome = Console.ReadLine();
+                        ListaDeNomes.Remove(ExcluirNome);
+
+
                         break;
 
                     case "4":
 
                         Console.WriteLine("Você escolheu a opção: " + Escolha + " - Pesquise um nome.");
+                        Console.WriteLine("Escreva o nome.");
+                        NomePesquisa = Console.ReadLine();
+
+                        foreach (var Nome in ListaDeNomes)
+                        {
+
+                            if (NomePesquisa == Nome)
+                            {
+                                NomeExiste = true;
+                                NomeEncontrado = Nome;
+
+
+                            }
+
+
+
+                        }
+
+                        if (NomeExiste)
+                        {
+                            NomeExiste = false;
+                            Console.WriteLine("Nome encontrado: " + NomeEncontrado);
+                            NomeEncontrado = "ND";
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Nome NÃO encontrado. ");
+                        }
 
                         break;
 
@@ -60,18 +135,37 @@
 
                         Console.WriteLine("Você escolheu a opção: " + Escolha + " - Consulte todos os nomes.");
 
+                        foreach (var Nome in ListaDeNomes)
+                        {
+
+                            Console.WriteLine(Nome);
+
+                        }
+
+                        break;
+
+
+                    case "7":
+
+                        for (int i = 0; i < 5; i++)
+                        {
+
+                            Console.WriteLine(ListaDeNomes[i]);
+
+                        }
+
                         break;
 
                     case "6":
 
-                        SAIR = false;
+                        NaoSair = false;
 
                         break;
 
                 }
                 Console.ReadKey();
                 Console.Clear();
-            }while (SAIR);
+            } while (NaoSair);
         }
     }
 }
--- Aprendendo Programação/obj/Debug/net8.0/Aprendendo Programação.AssemblyInfo.cs
+++ Aprendendo Programação/obj/Debug/net8.0/Aprendendo Programação.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aprendendo Programação")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3284f0c5bc2b9e0de64d45ed327dad6025d1aeba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49ae158bdac14f5906e1e80184ca1500f58b5466")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aprendendo Programação")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aprendendo Programação")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
